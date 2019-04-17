3a09afa1d6367b82b6d96d84f1a260578e953c58
Michal Kleczek <michal@kleczek.org>
2019-04-17 08:40:59 +0200

    Details should not extend Formattable

--- src/main/java/org/kleczek/io/http/exceptions/checked/Details.java
+++ src/main/java/org/kleczek/io/http/exceptions/checked/Details.java
@@ -1,9 +1,8 @@
 package org.kleczek.io.http.exceptions.checked;
 
-import java.util.Formattable;
 import java.util.Optional;
 
-public interface Details extends Formattable
+public interface Details
 {
 
     Optional<String> responseHeader(String string);
